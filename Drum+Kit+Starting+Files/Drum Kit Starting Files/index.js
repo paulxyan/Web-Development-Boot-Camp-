0ec562e3bf8df26643acfe94f10932b08712b7b8
Paul Yan <paulxyan@gmail.com>
2026-01-21 15:58:26 -0500

    Drum Kit project started

---
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,12 @@
+
+for (var i=0; i<document.querySelectorAll("button.drum").length; i++)
+
+document.querySelectAll("button.drum")[i].addEventListener("click", function () {
+    
+    this.style.color = "White";
+
+
+
+}   );
+
+
